4a27329f33916f7205fc1a466e2222b6f9696aeb
supminn <30731236+supminn@users.noreply.github.com>
2021-01-31 15:55:52 +0530

    fix: handled equal to condition

--- scripts/script.js
+++ scripts/script.js
@@ -5,27 +5,32 @@ const calculatePnL = () => {
     const purchasePrice = parseInt(document.querySelector("#purchase-price").value);
     const purchaseQty = parseInt(document.querySelector("#purchase-qty").value);
     const currentPrice = parseInt(document.querySelector("#current-price").value);
-    let flag = purchasePrice < currentPrice;
-    if (flag) {
-        //profit
-        let priceDifference = (currentPrice - purchasePrice);
-        let profitLoss = priceDifference * purchaseQty;
-        let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
-        console.log(profitLossPercentage + "% Profit of Rs" + profitLoss);
-        document.querySelector("#img-desktop-green").classList.remove('hidden');
-        document.querySelector("#img-desktop-red").classList.add('hidden');
-        document.querySelector(".container").style.background = "linear-gradient(to left, var(--green) 60%, var(--lightestCream) 60%)";
-        txtOutput.innerHTML = `🎉 You gained ${profitLossPercentage}% <br/>👍 Total profit of ₹${profitLoss}.`;
-    } else {
-        //loss
-        let priceDifference = (purchasePrice - currentPrice);
-        let profitLoss = priceDifference * purchaseQty;
-        let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
-        console.log(profitLossPercentage + "% Loss of Rs" + profitLoss);
-        document.querySelector("#img-desktop-green").classList.add('hidden');
-        document.querySelector("#img-desktop-red").classList.remove('hidden');
-        document.querySelector(".container").style.background = "linear-gradient(to left, var(--red) 60%, var(--lightestCream) 60%)";
-        txtOutput.innerHTML = `😞 You lost ${profitLossPercentage}% <br/>👎 Total loss of ₹${profitLoss}.`;
+    if (isNaN(purchasePrice) || isNaN(purchaseQty) || isNaN(currentPrice)) {
+        txtOutput.innerHTML = 'Inputs cannot be left blank, try again.'
+    }
+    else {
+        let flag = purchasePrice <= currentPrice;
+        if (flag) {
+            //profit
+            let priceDifference = (currentPrice - purchasePrice);
+            let profitLoss = priceDifference * purchaseQty;
+            let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
+            console.log(profitLossPercentage + "% Profit of Rs" + profitLoss);
+            document.querySelector("#img-desktop-green").classList.remove('hidden');
+            document.querySelector("#img-desktop-red").classList.add('hidden');
+            document.querySelector(".container").style.background = "linear-gradient(to left, var(--green) 60%, var(--lightestCream) 60%)";
+            txtOutput.innerHTML = `🎉 You gained ${profitLossPercentage}% <br/>👍 Total profit of ₹${profitLoss}.`;
+        } else {
+            //loss
+            let priceDifference = (purchasePrice - currentPrice);
+            let profitLoss = priceDifference * purchaseQty;
+            let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
+            console.log(profitLossPercentage + "% Loss of Rs" + profitLoss);
+            document.querySelector("#img-desktop-green").classList.add('hidden');
+            document.querySelector("#img-desktop-red").classList.remove('hidden');
+            document.querySelector(".container").style.background = "linear-gradient(to left, var(--red) 60%, var(--lightestCream) 60%)";
+            txtOutput.innerHTML = `😞 You lost ${profitLossPercentage}% <br/>👎 Total loss of ₹${profitLoss}.`;
+        }
     }
 }
 
