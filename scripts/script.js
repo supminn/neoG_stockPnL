bf69a83f992e25e38b247b5c7e3974b12171dd61
supminn <30731236+supminn@users.noreply.github.com>
2021-01-31 16:32:13 +0530

    style: loading spinner

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,48 @@
 const txtOutput = document.querySelector(".txt-output");
 const btnCheck = document.querySelector("#btn-check");
+const spinner = document.querySelector('#spinner');
 
-const calculatePnL = () => {
+
+const checkLoad = () => {
     const purchasePrice = parseInt(document.querySelector("#purchase-price").value);
     const purchaseQty = parseInt(document.querySelector("#purchase-qty").value);
     const currentPrice = parseInt(document.querySelector("#current-price").value);
     if (isNaN(purchasePrice) || isNaN(purchaseQty) || isNaN(currentPrice)) {
         txtOutput.innerHTML = 'Inputs cannot be left blank, try again.'
-    }
-    else {
-        let flag = purchasePrice <= currentPrice;
-        if (flag) {
-            //profit
-            let priceDifference = (currentPrice - purchasePrice);
-            let profitLoss = priceDifference * purchaseQty;
-            let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
-            console.log(profitLossPercentage + "% Profit of Rs" + profitLoss);
-            document.querySelector("#img-desktop-green").classList.remove('hidden');
-            document.querySelector("#img-desktop-red").classList.add('hidden');
-            document.querySelector(".container").style.background = "linear-gradient(to left, var(--green) 60%, var(--lightestCream) 60%)";
-            txtOutput.innerHTML = `🎉 You gained ${profitLossPercentage}% <br/>👍 Total profit of ₹${profitLoss}.`;
-        } else {
-            //loss
-            let priceDifference = (purchasePrice - currentPrice);
-            let profitLoss = priceDifference * purchaseQty;
-            let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
-            console.log(profitLossPercentage + "% Loss of Rs" + profitLoss);
-            document.querySelector("#img-desktop-green").classList.add('hidden');
-            document.querySelector("#img-desktop-red").classList.remove('hidden');
-            document.querySelector(".container").style.background = "linear-gradient(to left, var(--red) 60%, var(--lightestCream) 60%)";
-            txtOutput.innerHTML = `😞 You lost ${profitLossPercentage}% <br/>👎 Total loss of ₹${profitLoss}.`;
-        }
+    } else {
+        txtOutput.classList.add('hidden');
+        spinner.className = "show";
+        setTimeout(() => {
+            calculatePnL(purchasePrice, purchaseQty, currentPrice);
+        }, 2000);
     }
 }
 
-btnCheck.addEventListener('click', calculatePnL);
+const calculatePnL = (purchasePrice, purchaseQty, currentPrice) => {
+    spinner.className = spinner.className.replace("show", "");
+    txtOutput.classList.remove('hidden');
+    let flag = purchasePrice <= currentPrice;
+    if (flag) {
+        //profit
+        let priceDifference = (currentPrice - purchasePrice);
+        let profitLoss = priceDifference * purchaseQty;
+        let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
+        console.log(profitLossPercentage + "% Profit of Rs" + profitLoss);
+        document.querySelector("#img-desktop-green").classList.remove('hidden');
+        document.querySelector("#img-desktop-red").classList.add('hidden');
+        document.querySelector(".container").style.background = "linear-gradient(to left, var(--green) 60%, var(--lightestCream) 60%)";
+        txtOutput.innerHTML = `🎉 You gained ${profitLossPercentage}% <br/>👍 Total profit of ₹${profitLoss}.`;
+    } else {
+        //loss
+        let priceDifference = (purchasePrice - currentPrice);
+        let profitLoss = priceDifference * purchaseQty;
+        let profitLossPercentage = ((priceDifference / purchasePrice) * 100).toFixed(2);
+        console.log(profitLossPercentage + "% Loss of Rs" + profitLoss);
+        document.querySelector("#img-desktop-green").classList.add('hidden');
+        document.querySelector("#img-desktop-red").classList.remove('hidden');
+        document.querySelector(".container").style.background = "linear-gradient(to left, var(--red) 60%, var(--lightestCream) 60%)";
+        txtOutput.innerHTML = `😞 You lost ${profitLossPercentage}% <br/>👎 Total loss of ₹${profitLoss}.`;
+    }
+}
+
+btnCheck.addEventListener('click', checkLoad);
